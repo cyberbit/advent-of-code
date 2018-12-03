286c491380d8f02eaf4969f579467f94b64b95b5
Daniel Marcolesco <daniel@smashdevshop.com>
2018-12-02 22:07:21 -0600

    day 1 part 1

---
+++ src/components/days/2018/Day1.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="day-1">
+    <form @submit.prevent="calculate">
+      <div class="form-group">
+        <textarea
+          v-model="input"
+          class="form-control"
+          rows="15" />
+      </div>
+      <div class="form-group">
+        <button
+          type="submit"
+          class="btn btn-primary">Calculate</button>
+      </div>
+      <div class="form-group">
+        <label>The result is:</label>
+        <input
+          v-model="result"
+          type="text"
+          class="form-control"
+          readonly>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'Day1',
+  data () {
+    return {
+      input: '',
+      result: null
+    }
+  },
+  methods: {
+    calculate () {
+      let rows = _.split(this.input, '\n')
+
+      this.result = _.reduce(rows, (sum, v) => sum + Number(v), 0)
+    }
+  }
+}
+</script>
