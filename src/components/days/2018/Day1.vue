8747154b63ac5481a8d88a0792078fabad325441
Daniel Marcolesco <daniel@smashdevshop.com>
2018-12-02 22:47:17 -0600

    day 1 part 2

--- src/components/days/2018/Day1.vue
+++ src/components/days/2018/Day1.vue
@@ -13,9 +13,17 @@
           class="btn btn-primary">Calculate</button>
       </div>
       <div class="form-group">
-        <label>The result is:</label>
+        <label>The solution to part 1 is:</label>
         <input
-          v-model="result"
+          v-model="solution1"
+          type="text"
+          class="form-control"
+          readonly>
+      </div>
+      <div class="form-group">
+        <label>The solution to part 2 is:</label>
+        <input
+          v-model="solution2"
           type="text"
           class="form-control"
           readonly>
@@ -29,17 +37,47 @@ import _ from 'lodash'
 
 export default {
   name: 'Day1',
+
   data () {
     return {
       input: '',
-      result: null
+      solution1: null,
+      solution2: null
     }
   },
+
   methods: {
     calculate () {
+      this.part1()
+      this.part2()
+    },
+
+    part1 () {
       let rows = _.split(this.input, '\n')
 
-      this.result = _.reduce(rows, (sum, v) => sum + Number(v), 0)
+      this.solution1 = _.reduce(rows, (sum, v) => sum + Number(v), 0)
+    },
+
+    part2 () {
+      let rows = _.split(this.input, '\n')
+      let history = new Set()
+      let sum = 0
+      let done = false
+
+      while (!done) {
+        _.each(rows, (v) => {
+          sum += Number(v)
+
+          if (history.has(sum)) {
+            done = true
+            return false
+          } else {
+            history.add(sum)
+          }
+        })
+      }
+
+      this.solution2 = sum
     }
   }
 }
