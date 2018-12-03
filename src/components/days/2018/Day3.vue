e022e6a58842acaf139a98dabe11a36c639826ab
Daniel Marcolesco <daniel@smashdevshop.com>
2018-12-03 14:25:03 -0600

    day 3 part 2

--- src/components/days/2018/Day3.vue
+++ src/components/days/2018/Day3.vue
@@ -95,11 +95,40 @@ export default {
     },
 
     part2 () {
-      // let rows = _.split(this.input, '\n')
+      let rows = _.split(this.input, '\n')
+      let grid = _.times(MAX_DIM * MAX_DIM, _.constant(0))
+      let claims = []
 
-      //
+      _.each(rows, v => {
+        let [id, x, y, w, h] = _
+          .chain(v.match(/#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/)) // separate parts
+          .tail() // discard full match
+          .map(Number) // cast to numbers
+          .value() // destructure to variables
 
-      this.solution2 = null
+        claims.push({ id: id, over: 0 })
+
+        this.drawRect(x, y, w, h)
+
+        let origin = x + y * MAX_DIM // top left "point index" of claim
+
+        _.each(_.range(h), hh => {
+          _.each(_.range(w), ww => {
+            let point = origin + ww + hh * MAX_DIM // point index in grid
+
+            if (grid[point]) {
+              _.find(claims, ['id', grid[point]]).over++
+              _.find(claims, ['id', id]).over++
+            }
+
+            grid[point] = id // mark the claim
+          })
+        })
+      })
+
+      let solitary = _.find(claims, ['over', 0])
+
+      this.solution2 = solitary.id
     },
 
     drawRect (x, y, w, h) {
