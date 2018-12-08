b158b7fb054fee557a29fae54e32e6beb37571f4
D.J. Marcolesco <dj.marcolesco@gmail.com>
2018-12-08 15:47:24 -0600

    day 7 part 1

--- src/components/days/2018/Day7.vue
+++ src/components/days/2018/Day7.vue
@@ -53,10 +53,89 @@ export default {
 
     part1 () {
       let rows = _.split(this.input, '\n')
+      let pairs = []
+      let pairsRaw = _(rows)
+        .map(r => {
+          return _(r.match(/Step ([A-Z]).*step ([A-Z])/))
+            .tail()
+            .value()
+        })
+        .tap(pairsRawMapped => {
+          _.each(pairsRawMapped, pairRaw => {
+            let pairObj = _.reduce(['when', 'then'], (pair, prop, i) => ({ ...pair, [prop]: pairRaw[i] }), {})
 
-      //
+            pairs.push(pairObj)
+          })
+        })
+        .value()
 
-      this.solution1 = rows[null]
+      // let pairs
+
+      console.log(pairsRaw)
+      console.log(pairs)
+
+      let pairMap = _(pairsRaw)
+        .flatten()
+        .uniq()
+        .sort()
+        .reduce((c, v) => ({ ...c, [v]: new Set() }), {})
+
+      _.each(pairs, pair => {
+        pairMap[pair.then].add(pair.when)
+      })
+
+      console.log(pairMap)
+
+      let steps = []
+
+      let ct = 0
+      while (_.keys(pairMap).length && ct < 100) {
+        ct++
+
+        let waiting = _(pairMap)
+          .pickBy(['size', 0])
+          .reduce((arr, when, then) => [...arr, { then: then, when: when }], [])
+
+        let next = _.minBy(waiting, 'then')
+
+        // debugger
+
+        console.log(waiting, next)
+
+        steps.push(next)
+
+        _.each(pairMap, (map, i) => {
+          pairMap[i].delete(next.then)
+        })
+
+        _.unset(pairMap, next.then)
+      }
+
+      let stepString = _(steps)
+        .map('then')
+        .join('')
+
+      console.log(stepString)
+
+      /*
+      map rows to step pairs through regex match [when: match[0], then: match[1]]
+      pairMap = {}
+      steps = []
+      for pair in pairs
+        add pair.when to pairMap[pair.then]
+      end for pairs
+
+      loop until pairMap.length = 0
+        find maps with no steps
+        sort alphabetically
+        next = first step in sorted list
+        steps.push(next)
+        remove next from all maps
+        remove step from pairMap
+      end loop
+      */
+
+      this.solution1 = stepString
     },
 
     part2 () {
